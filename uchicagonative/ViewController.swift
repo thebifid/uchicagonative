c54dad0cc7e38c5bf98772d5d52d27eb5f0c4601
vasilymatveev <vasily.matveev@saritasa.com>
2020-07-21 12:25:37 +0700

    Initial Commit

---
+++ uchicagonative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uchicagonative
+//
+//  Created by Vasiliy Matveev on 21.07.2020.
+//  Copyright © 2020 Vasiliy Matveev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
