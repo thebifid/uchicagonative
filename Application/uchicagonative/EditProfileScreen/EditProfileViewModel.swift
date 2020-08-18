08031df940a69b59844b2c5e9328280655dec8cf
vasilymatveev <vasily.matveev@saritasa.com>
2020-08-18 15:21:52 +0700

    Fix access modifiers

--- Application/uchicagonative/EditProfileScreen/EditProfileViewModel.swift
+++ Application/uchicagonative/EditProfileScreen/EditProfileViewModel.swift
@@ -114,6 +114,26 @@ class EditProfileViewModel {
         }
     }
 
+    private func isInternetAvailable() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+
     private func isAllFieldsAreFill() -> Bool {
         let isFilled = !firstName.isEmpty && !lastName.isEmpty && birthYear != 0 && !gender.isEmpty && gender != genderList[0]
 
@@ -243,25 +263,5 @@ class EditProfileViewModel {
     func setProject(_ project: String) {
         self.project = project
         didUpdateState?()
-    }
-
-    func isInternetAvailable() -> Bool {
-        var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-
-        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
-                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-            }
-        }
-
-        var flags = SCNetworkReachabilityFlags()
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-            return false
-        }
-        let isReachable = flags.contains(.reachable)
-        let needsConnection = flags.contains(.connectionRequired)
-        return (isReachable && !needsConnection)
     }
 }
